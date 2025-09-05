db9a4df1076088fafcb1db4d3a20de99ff7c5d86
ankys8820 <ankys8820@outlook.com>
2025-09-05 23:58:11 +0530

    features : register form with query success.

--- server/requests.php
+++ server/requests.php
@@ -7,4 +7,23 @@ include("../common/db.php");
 
 // for signup
 if (isset($_POST['signup'])) {
+     $username = $_POST['username'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $password = $_POST['password'];
+
+    $user = $conn->prepare("INSERT INTO `users`
+    (`username`,`email`,`address`,`password`) VALUES
+    ('$username','$email','$address',$password)    
+    ");
+    $result = $user->execute();
+
+    // 
+    if($result){
+        echo "User added successfully!";
+        $_SESSION['user'] = ["username"=>$username, "email"=>$email]; 
+        header('location : /queries');
+    }else{
+        echo "some error occured!!";
+    }
 }
