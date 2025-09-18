157f661eedc00f2d22858e64c914e49b24440190
ankys8820 <ankys8820@outlook.com>
2025-09-18 15:49:53 +0530

    features :  added category name into question .

--- client/questions.php
+++ client/questions.php
@@ -14,11 +14,20 @@
     foreach ($result as $row) {
       $title = $row['title'];
       $id = $row['id'];
+      $category_id = $row['category_id'];
+
+           $sql = "SELECT * FROM `category` WHERE `id`='$category_id'";
+
+            $res = $conn->query($sql);
+            $category = $res->fetch_assoc();
+            $cat_name = $category['name'];
+
       echo "<div class='row question-list'>
              <h4>
-               <a class='question-list-questions' href='?q-id=$id'>
+               <a class='question-list-questions lead fs-2 ' href='?q-id=$id'>
                   $title
-               </a>              
+               </a>    
+               <span class='d-flex justify-content-end text-info'># $cat_name</span>          
              </h4>
         </div>";
     }
